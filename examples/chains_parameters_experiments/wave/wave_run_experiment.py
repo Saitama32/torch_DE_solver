e9767fa600c8d68c12b3ee6e4440432cb920b286
Saitama32 <rostik743@gmail.com>
2025-09-27 00:16:28 +0300

    Consolidate logging of error metrics in wave_run_experiment.py for improved readability

--- examples/chains_parameters_experiments/wave/wave_run_experiment.py
+++ examples/chains_parameters_experiments/wave/wave_run_experiment.py
@@ -194,17 +194,18 @@ def wave_1d_basic_experiment(experiment_args):
         (exact_func(grid).reshape(-1, 1) - net(grid)) ** 2) / torch.sum(exact_func(grid).reshape(-1, 1) ** 2))
     print(f"Train RMSE: {error_rmse_train}, L2RE: {error_l2re_train}")
 
-    experiment_args["experiment"].log_parameters("error_rmse_train", error_rmse_train.item())
-    experiment_args["experiment"].log_parameters("error_l2re_train", error_l2re_train.item())
-    
-
     error_rmse_test = torch.sqrt(torch.mean((exact_func(grid_test).reshape(-1, 1) - net(grid_test)) ** 2))
     error_l2re_test = torch.sqrt(torch.sum(
         (exact_func(grid_test).reshape(-1, 1) - net(grid_test)) ** 2) / torch.sum(exact_func(grid_test).reshape(-1, 1) ** 2))
     print(f"Test RMSE: {error_rmse_test}, L2RE: {error_l2re_test}")
     
-    experiment_args["experiment"].log_parameters("error_rmse_test", error_rmse_test.item())
-    experiment_args["experiment"].log_parameters("error_l2re_test", error_l2re_test.item())
+    experiment_args["experiment"].log_parameters({
+        "error_rmse_train", error_rmse_train.item(),
+        "error_l2re_train", error_l2re_train.item(),
+        "error_rmse_test", error_rmse_test.item(),
+        "error_l2re_test", error_l2re_test.item()
+    })
+
 
 
 
