7b006692b6646e3cf7b355824c1735271f8f233f
Saitama32 <rostik743@gmail.com>
2025-09-26 23:51:29 +0300

    Reduce epochs and max parallel jobs in PSO optimization parameters for improved performance

--- examples/chains_parameters_experiments/wave/wave_pso_optim_parameters.sh
+++ examples/chains_parameters_experiments/wave/wave_pso_optim_parameters.sh
@@ -11,11 +11,11 @@ num_res=10000
 opt=PSO
 lrs=(0.0001 0.001 0.0)
 pop_size=(20 30 40)
-epochs=15
+epochs=10
 betas=(5)
 devices=(0)
 proj=wave_pso_parameters
-max_parallel_jobs=5
+max_parallel_jobs=3
 
 background_pids=()
 current_device=0
