58143a3d5efe28ba56ddce20b5ff77389f83f721
Saitama32 <rostik743@gmail.com>
2025-10-02 16:11:21 +0300

    Change pop size list and lrs order

--- examples/chains_parameters_experiments/wave/wave_pso_optim_parameters.sh
+++ examples/chains_parameters_experiments/wave/wave_pso_optim_parameters.sh
@@ -9,13 +9,13 @@ num_x=257
 num_t=101
 num_res=10000
 opt=PSO
-lrs=(0.0001 0.001 0.0)
-pop_size=(20 30 40)
+lrs=(0.0 0.0001 0.001)
+pop_size=(30 50 100)
 epochs=1500
 betas=(5)
 devices=(0)
 proj=wave_pso_parameters
-max_parallel_jobs=2
+max_parallel_jobs=3
 
 background_pids=()
 current_device=0
