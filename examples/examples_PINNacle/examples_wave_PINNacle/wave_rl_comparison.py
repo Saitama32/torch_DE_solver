11f8523c3074f1c4caf06cf2056bd47f632ac18c
Saitama32 <rostik743@gmail.com>
2025-10-11 18:27:42 +0300

    Add new experiment key for load model

--- examples/examples_PINNacle/examples_wave_PINNacle/wave_rl_comparison.py
+++ examples/examples_PINNacle/examples_wave_PINNacle/wave_rl_comparison.py
@@ -281,7 +281,7 @@ def wave_1d_basic_experiment(seed, x_res, t_res, beta=5):
     comparison_params = {
         "seed": seed, 
         "total_epochs": 5000,
-        "experiment_key": "4ee79b89c2eb4658856e9720cf35a34a"
+        "experiment_key": "9da803bf471942d68069d835e2f95651"
     }
 
     experiment.log_parameters(rl_agent_params)
