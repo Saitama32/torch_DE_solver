66b58dcb1f25900e6f7f8822de9c4b9ffa4302af
Saitama32 <rostik743@gmail.com>
2025-11-07 02:07:24 +0300

    add new backup exp key

--- examples/examples_PINNacle/examples_burgers_PINNacle/burgers_rl_chain.py
+++ examples/examples_PINNacle/examples_burgers_PINNacle/burgers_rl_chain.py
@@ -248,13 +248,13 @@ def burgers_1d_experiment(x_res, t_res):
         "exp": experiment,
     }
 
-    # backup_params = {
-    #     "experiment_key" : "faa19ed1bd5b4ad58b94b20db648fc78",
-    # }
-    backup_params = None
+    backup_params = {
+        "experiment_key" : "a5c2121355bb42afb5e338d4a5b9c5c0",
+    }
+    # backup_params = None
 
     experiment.log_parameters(rl_agent_params)
-    # experiment.log_parameters(backup_params)
+    experiment.log_parameters(backup_params)
 
     model.train(optimizer,
                 5e5,
