17e989803c9d6477f0cacd504d74bab811316a06
Saitama32 <rostik743@gmail.com>
2025-11-07 17:36:49 +0300

    add heat_3d_rl_chain epxeriment

---
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_3d_rl_chain.py
@@ -0,0 +1,312 @@
+from comet_ml import start
+from comet_ml.integration.pytorch import log_model
+
+experiment = start(
+  api_key="aP71fQTYPNqfsYWvudPPmoBl5",
+  project_name="rlpinn_heat_3d_tolerance",
+  workspace="saitama32"
+)
+
+
+import torch
+import os
+import sys
+import time
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tedeous.data import Domain, Conditions, Equation
+from tedeous.model import Model
+from tedeous.callbacks import cache, early_stopping, plot
+from tedeous.optimizers.optimizer import Optimizer
+from tedeous.device import solver_device
+
+experiment.log_parameters({
+    "param": "v_1",
+    "reward_function": "v_2",
+    "description": "farm_transitions_Heat_3d_basic_RL_optimizer"
+})
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+solver_device(device)
+
+eps = 1
+N = 5
+k = torch.arange(N)
+
+
+def exact_func(grid):
+    x, y, z, t = grid[:, 0], grid[:, 1], grid[:, 2], grid[:, 3]
+    sln = torch.sum((torch.sin(k * x[:, None]) +
+                     torch.sin(k * y[:, None]) +
+                     torch.sin(k * z[:, None])) *
+                    torch.exp(-eps * k ** 3 * t[:, None]))
+    return sln
+
+
+def heat_3d_analytic_experiment(grid_res):
+    exp_dict_list = []
+
+    x_min, x_max = 0, 2 * torch.pi
+    y_min, y_max = 0, 2 * torch.pi
+    z_min, z_max = 0, 2 * torch.pi
+    t_max = 0.01
+
+    pde_dim_in = 4
+    pde_dim_out = 1
+
+    domain = Domain()
+
+    domain.variable('x', [x_min, x_max], grid_res)
+    domain.variable('y', [y_min, y_max], grid_res)
+    domain.variable('z', [z_min, z_max], grid_res)
+    domain.variable('t', [0, t_max], 10)
+
+    domain_test = Domain()
+    grid_test_res = 80
+
+    domain_test.variable('x', [x_min, x_max], grid_test_res)
+    domain_test.variable('y', [y_min, y_max], grid_test_res)
+    domain_test.variable('z', [z_min, z_max], grid_test_res)
+    domain_test.variable('t', [0, t_max], 10)
+
+
+    boundaries = Conditions()
+
+    # Initial condition: ###############################################################################################
+
+    init_func = lambda grid: torch.sum(torch.sin(k * grid[:, 0][:, None]) +
+                                       torch.sin(k * grid[:, 1][:, None]) +
+                                       torch.sin(k * grid[:, 2][:, None]))
+
+    # u(x, y, 0)
+    boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 'z': [z_min, z_max], 't': 0}, value=init_func)
+
+    # Boundary conditions (periodic): ##################################################################################
+
+    # u(0, y, z, t) = u(2*pi, y, z, t)
+    boundaries.periodic([{'x': x_min, 'y': [y_min, y_max], 'z': [z_min, z_max], 't': [0, t_max]},
+                         {'x': x_max, 'y': [y_min, y_max], 'z': [z_min, z_max], 't': [0, t_max]}])
+
+    # u(x, 0, z, t) = u(x, 2*pi, z, t)
+    boundaries.periodic([{'x': [x_min, x_max], 'y': y_min, 'z': [z_min, z_max], 't': [0, t_max]},
+                         {'x': [x_min, x_max], 'y': y_max, 'z': [z_min, z_max], 't': [0, t_max]}])
+
+    # u(x, y, 0, t) = u(x, y, 2*pi, t)
+    boundaries.periodic([{'x': [x_min, x_max], 'y': [y_min, y_max], 'z': z_min, 't': [0, t_max]},
+                         {'x': [x_min, x_max], 'y': [y_min, y_max], 'z': z_max, 't': [0, t_max]}])
+
+    equation = Equation()
+
+    # Operator: du/dt -  epsilon * (u_xx + u_yy + u_zz) = 0
+
+    heat_3d_analytic = {
+        'du/dt**1':
+            {
+                'coeff': 1,
+                'term': [3],
+                'pow': 1,
+                'var': 0
+            },
+        '-epsilon * d2u/dx2**1':
+            {
+                'coeff': -eps,
+                'term': [0, 0],
+                'pow': 1,
+                'var': 0
+            },
+        '-epsilon * d2u/dy2**1':
+            {
+                'coeff': -eps,
+                'term': [1, 1],
+                'pow': 1,
+                'var': 0
+            },
+        '-epsilon * d2u/dz2**1':
+            {
+                'coeff': -eps,
+                'term': [2, 2],
+                'pow': 1,
+                'var': 0
+            }
+    }
+
+    equation.add(heat_3d_analytic)
+
+    neurons = 100
+
+    net = torch.nn.Sequential(
+        torch.nn.Linear(pde_dim_in, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, pde_dim_out)
+    )
+
+    for m in net.modules():
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_normal_(m.weight)
+            torch.nn.init.zeros_(m.bias)
+
+    start = time.time()
+
+    model_layers = [pde_dim_in, neurons, neurons, neurons, neurons, pde_dim_out]
+
+    grid = domain.build('NN').to(device)
+    grid_test = domain_test.build('NN').to(device)
+
+    model = Model(net, domain, equation, boundaries)
+
+    
+    model.compile('autograd', lambda_operator=1, lambda_bound=100)
+    u_exact_test = exact_func(grid_test).reshape(-1)
+
+    equation_params = [u_exact_test, grid_test, grid, domain, equation, boundaries, model_layers]
+
+    img_dir = os.path.join(os.path.dirname(__file__), 'heat_3d_analytic_img')
+
+
+    cb_es = early_stopping.EarlyStopping(eps=1e-6,
+                                         loss_window=100,
+                                         no_improvement_patience=1000,
+                                         patience=5,
+                                         randomize_parameter=1e-6,
+                                         info_string_every=10)
+
+    optimizer = {
+        'Adam':{
+            'lr':[1e-2, 1e-3, 1e-4],
+            'epochs':[100, 1000, 2500]
+        },
+        'LBFGS':{
+            'lr':[1, 5e-1, 1e-1],
+            'epochs':[100, 500, 1500]
+        },
+        'PSO':{
+            'lr':[0.0, 1e-3, 1e-4],
+            'epochs':[100, 200, 300]
+        },
+    }
+
+    AE_model_params = {
+        "mode": "NN",
+        "num_of_layers": 3,
+        "layers_AE": [
+            991,
+            125,
+            15
+        ],
+        "num_models": None,
+        "from_last": False,
+        "prefix": "model-",
+        "every_nth": 1,
+        "grid_step": 0.1,
+        "d_max_latent": 2,
+        "anchor_mode": "circle",
+        "rec_weight": 10000.0,
+        "anchor_weight": 0.0,
+        "lastzero_weight": 0.0,
+        "polars_weight": 0.0,
+        "wellspacedtrajectory_weight": 0.0,
+        "gridscaling_weight": 0.0,
+        "device": device
+    }
+
+    AE_train_params = {
+        "first_RL_epoch_AE_params": {
+            "epochs": 10000,
+            "patience_scheduler": 4000,
+            "cosine_scheduler_patience": 1200,
+        },
+        "other_RL_epoch_AE_params": {
+            "epochs": 20000,
+            "patience_scheduler": 4000,
+            "cosine_scheduler_patience": 1200,
+        },
+        "batch_size": 32,
+        "every_epoch": 100,
+        "learning_rate": 5e-4,
+        "resume": True,
+        "finetune_AE_model": False
+    }
+
+    loss_surface_params = {
+        "loss_types": ["loss_total", "loss_oper", "loss_bnd"],
+        "every_nth": 1,
+        "num_of_layers": 3,
+        "layers_AE": [
+            991,
+            125,
+            15
+        ],
+        "batch_size": 32,
+        "num_models": None,
+        "from_last": False,
+        "prefix": "model-",
+        "loss_name": "loss_total",
+        "x_range": [-1.25, 1.25, 25],
+        "vmax": -1.0,
+        "vmin": -1.0,
+        "vlevel": 30.0,
+        "key_models": None,
+        "key_modelnames": None,
+        "density_type": "CKA",
+        "density_p": 2,
+        "density_vmax": -1,
+        "density_vmin": -1,
+        "colorFromGridOnly": True,
+        "img_dir": img_dir
+    }
+
+    rl_agent_params = {
+        "n_save_models": 10,
+        "n_trajectories": 1000,
+        "tolerance": 0.00001, 
+        "stuck_threshold": 10,  # Число эпох без значительного изменения прогресса
+        "min_loss_change": 1e-7,
+        "min_grad_norm": 1e-5,
+        "rl_buffer_size": 10000,
+        "rl_batch_size": 32,
+        "n_transitions_reinit" : 2000,
+        "gamma": 0.9,
+        "rl_reward_method": "absolute",
+        "exact_solution": exact_func,
+        "reward_operator_coeff": 1,
+        "reward_boundary_coeff": 1,
+        "lr": 1e-3,
+        "exp": experiment,
+    }
+
+    # backup_params = {
+    #     "experiment_key" : "b0dae86c42924e4484b8bd194e2d58d9",
+    # }
+    backup_params = None
+
+    experiment.log_parameters(rl_agent_params)
+    # experiment.log_parameters(backup_params)
+
+    model.train(optimizer,
+                5e5,
+                save_model=True,
+                callbacks=[cb_es],
+                rl_agent_params=rl_agent_params,
+                models_concat_flag=False,
+                model_name='rl_optimization_agent',
+                equation_params=equation_params,
+                AE_model_params=AE_model_params,
+                AE_train_params=AE_train_params,
+                loss_surface_params=loss_surface_params,
+                backup_params=backup_params)
+
+    return exp_dict_list
+
+
+if __name__ == "__main__":
+    grid_res = 100
+
+    exp_dict_list = heat_3d_analytic_experiment(grid_res)
