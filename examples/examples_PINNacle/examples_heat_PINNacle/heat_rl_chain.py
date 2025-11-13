d8234f27f2dc3ef03948fad141e50353b056e7b5
Saitama32 <rostik743@gmail.com>
2025-11-13 14:40:54 +0300

    Change params to continue training

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
@@ -252,7 +252,7 @@ def heat_2d_long_time_experiment(grid_res):
         "min_loss_change": 1e-7,
         "min_grad_norm": 1e-5,
         "rl_buffer_size": 10000,
-        "rl_batch_size": 64,
+        "rl_batch_size": 32,
         "n_transitions_reinit" : 2000,
         "gamma": 0.9,
         "rl_reward_method": "absolute",
@@ -263,10 +263,10 @@ def heat_2d_long_time_experiment(grid_res):
         "exp": experiment,
     }
 
-    # backup_params = {
-    #     "experiment_key" : "859da27afb394c83aefe85dc417250bc",
-    # }
-    backup_params = None
+    backup_params = {
+        "experiment_key" : "d6271fc4f6684ffb8c4d30b60f0da860",
+    }
+    # backup_params = None
     experiment.log_parameters(rl_agent_params)
     experiment.log_parameters(backup_params)
 
