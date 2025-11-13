3c45ddcee2bf61fc8a09d629f6e0946e2e21808d
Saitama32 <rostik743@gmail.com>
2025-11-13 16:50:04 +0300

    Change comparison key to correct_clementine

--- examples/examples_PINNacle/examples_burgers_PINNacle/burgers_rl_comparison.py
+++ examples/examples_PINNacle/examples_burgers_PINNacle/burgers_rl_comparison.py
@@ -255,7 +255,7 @@ def burgers_1d_experiment(x_res, t_res):
     comparison_params = {
         "seed": seed, 
         "total_epochs": 5000,
-        "experiment_key": "d54cf3ffddb64a168eef5e0c0581295c"
+        "experiment_key": "8ddf76ade2ab4f10aa55a4257162d109"
     }
 
     experiment.log_parameters(rl_agent_params)
