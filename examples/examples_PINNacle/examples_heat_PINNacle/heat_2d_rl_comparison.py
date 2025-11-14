f0495a0fcc7aab74420fe80d8ed316f9c82d7f21
Saitama32 <rostik743@gmail.com>
2025-11-14 16:25:07 +0300

    change exp key to loose_stale

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_rl_comparison.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_rl_comparison.py
@@ -268,7 +268,7 @@ def heat_2d_long_time_experiment(grid_res):
     comparison_params = {
         "seed": seed, 
         "total_epochs": 5000,
-        "experiment_key": "6a6a7c5260454495beaa456bdbe1ded7"
+        "experiment_key": "631766555d824152a55b521440bc896f"
     }
 
     experiment.log_parameters(rl_agent_params)
