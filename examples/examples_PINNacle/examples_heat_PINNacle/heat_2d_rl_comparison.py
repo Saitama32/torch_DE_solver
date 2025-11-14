33b12aded3ef25cf8190e5e75fa44d1276eb3642
Saitama32 <rostik743@gmail.com>
2025-11-14 16:23:30 +0300

    Change exp key to key_firestop

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_rl_comparison.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_rl_comparison.py
@@ -268,7 +268,7 @@ def heat_2d_long_time_experiment(grid_res):
     comparison_params = {
         "seed": seed, 
         "total_epochs": 5000,
-        "experiment_key": "d8d3331849164b6b8516d36aa7e4aa54"
+        "experiment_key": "6a6a7c5260454495beaa456bdbe1ded7"
     }
 
     experiment.log_parameters(rl_agent_params)
