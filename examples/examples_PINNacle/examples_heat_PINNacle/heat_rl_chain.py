07640745c1c1626151e481c08f9261a5797921e0
Saitama32 <rostik743@gmail.com>
2025-11-24 20:02:27 +0300

    debug backup_params = None

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
@@ -267,7 +267,7 @@ def heat_2d_long_time_experiment(grid_res):
     # backup_params = {
     #     "experiment_key" : "2544e8a0094442a59bdef65a1752af98",
     # }
-    # backup_params = None
+    backup_params = None
     experiment.log_parameters(rl_agent_params)
     # experiment.log_parameters(backup_params)
 
