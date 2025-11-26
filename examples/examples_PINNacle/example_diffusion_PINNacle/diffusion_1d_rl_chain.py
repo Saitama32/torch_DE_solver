f5fb44eef34956e181f9fc73e9b1081ab042b839
Saitama32 <rostik743@gmail.com>
2025-11-26 16:28:07 +0300

    Change project key

--- examples/examples_PINNacle/example_diffusion_PINNacle/diffusion_1d_rl_chain.py
+++ examples/examples_PINNacle/example_diffusion_PINNacle/diffusion_1d_rl_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_diffusion_1d_optimization",
+  project_name="rlpinn_diffusion_1d_optimization_abs_reward",
   workspace="saitama32"
 )
 
