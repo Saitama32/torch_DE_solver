d3af1c27b5c1c018d69c9e2fbf17565a62e2292e
Saitama32 <rostik743@gmail.com>
2025-11-26 16:23:21 +0300

    Change project key

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_rl_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_heat_2d_final",
+  project_name="rlpinn_heat_2d_final_abs_reward",
   workspace="saitama32"
 )
 
