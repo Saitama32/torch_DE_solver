b01cdd8728df97b787d025e825b9282089999b84
Saitama32 <rostik743@gmail.com>
2025-12-11 16:12:35 +0300

    Change project name for wave

--- examples/examples_PINNacle/examples_wave_PINNacle/wave_rl_comparison.py
+++ examples/examples_PINNacle/examples_wave_PINNacle/wave_rl_comparison.py
@@ -9,7 +9,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_comparison",
+  project_name="rlpinn_wave_comparison",
   workspace="saitama32"
 )
 
