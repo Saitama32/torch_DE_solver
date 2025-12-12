ff4a4c77d96949356fd2717249fc551f05248cfc
Saitama32 <rostik743@gmail.com>
2025-12-12 17:53:18 +0300

    Add heat random rl implementation

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_random_init_rl_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_random_init_rl_chain.py
@@ -1,3 +1,12 @@
+from comet_ml import start
+from comet_ml.integration.pytorch import log_model
+
+experiment = start(
+  api_key="aP71fQTYPNqfsYWvudPPmoBl5",
+  project_name="rlpinn_heat_2d_random_tolerance",
+  workspace="saitama32"
+)
+
 import torch
 import os
 import sys
@@ -12,7 +21,14 @@ from tedeous.callbacks import cache, early_stopping, plot
 from tedeous.optimizers.optimizer import Optimizer
 from tedeous.device import solver_device
 
-solver_device('gpu')
+experiment.log_parameters({
+    "param": "v_1",
+    "reward_function": "v_2",
+    "description": "farm_transitions_Heat_2d_1d_basic_RL_optimizer"
+})
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+solver_device(device)
 
 eps = 1.0
 
@@ -168,6 +184,13 @@ def heat_2d_gaussian_init_experiment(grid_res, seed=None):
     domain.variable('y', [y_min, y_max], grid_res)
     domain.variable('t', [0, t_max], 6)
 
+    domain_test = Domain()
+    grid_test_res = 80
+
+    domain_test.variable('x', [x_min, x_max], grid_test_res)
+    domain_test.variable('y', [y_min, y_max], grid_test_res)
+    domain_test.variable('t', [0, t_max], 6)
+
     boundaries = Conditions()
 
     init_func, exact_func = make_gaussian_init(Kmax=6, seed=seed, device='cuda')
@@ -235,72 +258,158 @@ def heat_2d_gaussian_init_experiment(grid_res, seed=None):
             torch.nn.init.xavier_normal_(m.weight)
             torch.nn.init.zeros_(m.bias)
 
+    model_layers = [pde_dim_in, neurons, neurons, neurons, neurons, neurons, pde_dim_out]
+
+    grid = domain.build('NN').to(device)
+    grid_test = domain_test.build('NN').to(device)
+
     start = time.time()
 
     model = Model(net, domain, equation, boundaries)
 
     model.compile('autograd', lambda_operator=1, lambda_bound=100)
+    u_exact_test = exact_func(grid_test).reshape(-1)
+    
+    equation_params = [u_exact_test, grid_test, grid, domain, equation, boundaries, model_layers]
 
     img_dir = os.path.join(os.path.dirname(__file__), 'heat_2d_gaussian_init_img')
 
-    cb_cache = cache.Cache(cache_verbose=False, model_randomize_parameter=1e-6)
 
     cb_es = early_stopping.EarlyStopping(eps=1e-6,
                                          loss_window=100,
                                          no_improvement_patience=1000,
                                          patience=10,
-                                         randomize_parameter=1e-6,
+                                         randomize_parameter=1e-4,
                                          info_string_every=10)
 
-    cb_plots = plot.Plots(save_every=500,
-                          print_every=None,
-                          img_dir=img_dir,
-                          img_dim='2d',
-                          scatter_flag=False,
-                          plot_axes=[0, 1],
-                          fixed_axes=[2],
-                          n_samples=4,
-                          img_rows=2,
-                          img_cols=2)
+    optimizer = {
+        'Adam':{
+            'lr':[1e-2, 1e-3, 1e-4],
+            'epochs':[100, 1000, 2500]
+        },
+        'LBFGS':{
+            'lr':[1, 5e-1, 1e-1],
+            'epochs':[100, 500, 1500]
+        },
+        'PSO':{
+            'lr':[0.0, 1e-3, 1e-4],
+            'epochs':[100, 200, 300]
+        },
+    }
 
-    optimizer = Optimizer('Adam', {'lr': 5e-4})
+    AE_model_params = {
+        "mode": "NN",
+        "num_of_layers": 3,
+        "layers_AE": [
+            991,
+            125,
+            15
+        ],
+        "num_models": None,
+        "from_last": False,
+        "prefix": "model-",
+        "every_nth": 1,
+        "grid_step": 0.1,
+        "d_max_latent": 2,
+        "anchor_mode": "circle",
+        "rec_weight": 10000.0,
+        "anchor_weight": 0.0,
+        "lastzero_weight": 0.0,
+        "polars_weight": 0.0,
+        "wellspacedtrajectory_weight": 0.0,
+        "gridscaling_weight": 0.0,
+        "device": device
+    }
 
-    callbacks = [cb_cache, cb_es, cb_plots]
+    AE_train_params = {
+        "first_RL_epoch_AE_params": {
+            "epochs": 10000,
+            "patience_scheduler": 4000,
+            "cosine_scheduler_patience": 1200,
+        },
+        "other_RL_epoch_AE_params": {
+            "epochs": 20000,
+            "patience_scheduler": 4000,
+            "cosine_scheduler_patience": 1200,
+        },
+        "batch_size": 32,
+        "every_epoch": 100,
+        "learning_rate": 5e-4,
+        "resume": True,
+        "finetune_AE_model": False
+    }
 
-    model.train(optimizer, 1e5, save_model=False, callbacks=callbacks)
+    loss_surface_params = {
+        "loss_types": ["loss_total", "loss_oper", "loss_bnd"],
+        "every_nth": 1,
+        "num_of_layers": 3,
+        "layers_AE": [
+            991,
+            125,
+            15
+        ],
+        "batch_size": 32,
+        "num_models": None,
+        "from_last": False,
+        "prefix": "model-",
+        "loss_name": "loss_total",
+        "x_range": [-1.25, 1.25, 25],
+        "vmax": -1.0,
+        "vmin": -1.0,
+        "vlevel": 30.0,
+        "key_models": None,
+        "key_modelnames": None,
+        "density_type": "CKA",
+        "density_p": 2,
+        "density_vmax": -1,
+        "density_vmin": -1,
+        "colorFromGridOnly": True,
+        "img_dir": img_dir
+    }
 
-    end = time.time()
+    rl_agent_params = {
+        "n_save_models": 10,
+        "n_trajectories": 1000,
+        "tolerance": 0.0608023181557655,
+        "prev_tol": 0.060776,
+        "stuck_threshold": 10,  # Число эпох без значительного изменения прогресса
+        "min_loss_change": 1e-7,
+        "min_grad_norm": 1e-5,
+        "rl_buffer_size": 10000,
+        "rl_batch_size": 32,
+        "n_transitions_reinit" : 2000,
+        "gamma": 0.9,
+        "rl_reward_method": "absolute",
+        "exact_solution": exact_func,
+        "reward_operator_coeff": 1,
+        "reward_boundary_coeff": 1,
+        "lr": 1e-3,
+        "exp": experiment,
+    }
 
-    grid = domain.build('NN').to('cuda')
-    net = net.to('cuda')
+    # backup_params = {
+    #     "experiment_key" : "b0dae86c42924e4484b8bd194e2d58d9",
+    # }
+    backup_params = None
+    experiment.log_parameters(rl_agent_params)
+    # experiment.log_parameters(backup_params)
 
-    exact_on_grid = exact_func(grid)
-    pred = net(grid)
-    error_rmse = torch.sqrt(torch.mean((exact_on_grid - pred) ** 2))
-
-    exp_dict_list.append({
-        'grid_res': grid_res,
-        'time': end - start,
-        'RMSE': error_rmse.detach().cpu().numpy(),
-        'type': 'heat_2d_gaussian_init',
-        'cache': True
-    })
-
-    print('Time taken {}= {}'.format(grid_res, end - start))
-    print('RMSE {}= {}'.format(grid_res, error_rmse))
+    model.train(optimizer,
+                5e5,
+                save_model=True,
+                callbacks=[cb_es],
+                rl_agent_params=rl_agent_params,
+                models_concat_flag=False,
+                model_name='rl_optimization_agent',
+                equation_params=equation_params,
+                AE_model_params=AE_model_params,
+                AE_train_params=AE_train_params,
+                loss_surface_params=loss_surface_params,
+                backup_params=backup_params)
 
     return exp_dict_list
 
+if __name__ == "__main__":
+    grid_res = 100
 
-nruns = 1
-
-exp_dict_list = []
-for grid_res in range(100, 1001, 100):
-    for r in range(nruns):
-        exp_dict_list.append(heat_2d_gaussian_init_experiment(grid_res, seed=r))
-
-import pandas as pd
-
-exp_dict_list_flatten = [item for sublist in exp_dict_list for item in sublist]
-df = pd.DataFrame(exp_dict_list_flatten)
-df.to_csv('examples/benchmarking_data/heat_2d_gaussian_init_experiment.csv')
+    exp_dict_list = heat_2d_gaussian_init_experiment(grid_res)
