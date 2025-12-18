b2f1e04c51a594cdef8f4438cb72a85926177bd5
Saitama32 <rostik743@gmail.com>
2025-12-18 17:30:13 +0300

    add prev tol

--- examples/examples_PINNacle/example_diffusion_PINNacle/convection_diffusion_1d_rl_chain.py
+++ examples/examples_PINNacle/example_diffusion_PINNacle/convection_diffusion_1d_rl_chain.py
@@ -241,7 +241,8 @@ def convection_diffusion_1d_experiment(grid_res):
     rl_agent_params = {
         "n_save_models": 10,
         "n_trajectories": 1000,
-        "tolerance": 0.0, 
+        "tolerance": 0.0,
+        "prev_tol": 0.0, 
         "stuck_threshold": 10,  # Число эпох без значительного изменения прогресса
         "min_loss_change": 1e-7,
         "min_grad_norm": 1e-5,
