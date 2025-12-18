79fa277ae54a417b09ad4a2c805e7b26a5758442
Saitama32 <rostik743@gmail.com>
2025-12-19 00:17:23 +0300

    Change compare key diffusion

--- examples/examples_PINNacle/example_diffusion_PINNacle/diffusion_1d_rl_comparison.py
+++ examples/examples_PINNacle/example_diffusion_PINNacle/diffusion_1d_rl_comparison.py
@@ -254,7 +254,7 @@ def diffusion_1d_experiment(grid_res):
     comparison_params = {
         "seed": seed, 
         "total_epochs": 7000,
-        "experiment_key": "7c7cd9595c6c4d54abe2a173fc68e347",
+        "experiment_key": "47c5550167ab4d96b1ac80b0335f27a6",
         "grid_res": grid_res
     }
 
