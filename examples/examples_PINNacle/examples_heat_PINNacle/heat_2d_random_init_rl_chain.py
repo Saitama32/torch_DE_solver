9c5c385c8d583c8a6d3e0550b6f18984ea5fe23b
Saitama32 <rostik743@gmail.com>
2025-12-21 21:53:23 +0300

    Change proj name for main file

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_random_init_rl_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_random_init_rl_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_heat_2d_random_farm_trans",
+  project_name="rlpinn_heat_2d_random_optimization",
   workspace="saitama32"
 )
 
