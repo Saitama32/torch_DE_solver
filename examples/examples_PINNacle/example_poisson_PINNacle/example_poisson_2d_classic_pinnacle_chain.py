47aebc95b869c6478e5f16207c138275ea38d9b9
Saitama32 <rostik743@gmail.com>
2026-01-06 22:20:57 +0300

    Change project name

--- examples/examples_PINNacle/example_poisson_PINNacle/example_poisson_2d_classic_pinnacle_chain.py
+++ examples/examples_PINNacle/example_poisson_PINNacle/example_poisson_2d_classic_pinnacle_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_heat_2d_random_tolerance",
+  project_name="rlpinn_poisson_2d_classic_tolerance",
   workspace="saitama32"
 )
 
