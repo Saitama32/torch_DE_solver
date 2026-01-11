9a9579ee93109d0ba7c3d8470c0e9abda9333158
Saitama32 <rostik743@gmail.com>
2026-01-11 18:25:08 +0300

    Chnage project name

--- examples/examples_PINNacle/example_poisson_PINNacle/example_poisson_2d_classic_pinnacle_chain.py
+++ examples/examples_PINNacle/example_poisson_PINNacle/example_poisson_2d_classic_pinnacle_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_poisson_2d_classic_farm_transitions",
+  project_name="rlpinn_poisson_2d_classic_optimization",
   workspace="saitama32"
 )
 
