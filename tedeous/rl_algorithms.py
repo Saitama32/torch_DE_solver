a7a8d4197be5247d82cdc555ae806e36aeda0e55
Saitama32 <rostik743@gmail.com>
2026-01-23 17:52:25 +0300

    Add poisson_2d_cg for tol

--- tedeous/rl_algorithms.py
+++ tedeous/rl_algorithms.py
@@ -22,9 +22,10 @@ from tedeous.RL_utils.logger import log_priority_to_comet
 import tempfile
 
 
-EPS_START = 0.5
+GAMMA = 0.95
+EPS_START = 0.95
 EPS_END = 0.05
-EPS_DECAY = 300
+EPS_DECAY = 1000
 TAU = 0.01
 
 
