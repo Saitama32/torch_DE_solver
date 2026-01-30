f80bbcb51aa5dd531b95a6f01524d648f0d5dfcf
Saitama32 <rostik743@gmail.com>
2026-01-30 03:18:40 +0300

    change proj name

--- examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_cg_chain.py
+++ examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_cg_chain.py
@@ -3,7 +3,7 @@ from comet_ml.integration.pytorch import log_model
 
 experiment = start(
   api_key="aP71fQTYPNqfsYWvudPPmoBl5",
-  project_name="rlpinn_poisson_2d_farm_transitions",
+  project_name="rlpinn_poisson_2d_cg_farm_transitions",
   workspace="saitama32"
 )
 
