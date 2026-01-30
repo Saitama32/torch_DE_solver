7499cfbe67840da5e1a2e4c1951a332825a669bf
Saitama32 <rostik743@gmail.com>
2026-01-30 23:58:49 +0300

    reducing the load on the card

--- examples/examples_PINNacle/example_kuramoto_sivashinsky_PINNacle/kuramoto_sivashinsky.sh
+++ examples/examples_PINNacle/example_kuramoto_sivashinsky_PINNacle/kuramoto_sivashinsky.sh
@@ -15,19 +15,14 @@ fi
 
 if [ "$NUM_GPUS" -eq 1 ]; then
     echo "Запускаем 2 процесса на одной GPU..."
-    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
-    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" 
 elif [ "$NUM_GPUS" -ge 2 ]; then
     echo "Запускаем по 2 процесса на каждую из двух GPU..."
     CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
-    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
-    CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
     CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
 else
     echo "⚠️ Найдено более 2 GPU, но используется только первые две."
     CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
-    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
-    CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
     CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
 fi
 
