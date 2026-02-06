e2ce1a1a3a6d4bf71effde61991f89967595c26a
Saitama32 <rostik743@gmail.com>
2026-02-07 00:07:52 +0300

    Change grid res

--- examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_long_time_chain.py
+++ examples/examples_PINNacle/examples_heat_PINNacle/heat_2d_long_time_chain.py
@@ -295,6 +295,6 @@ def heat_2d_long_time_experiment(grid_res):
     return exp_dict_list
 
 if __name__ == "__main__":
-    grid_res = 80
+    grid_res = 50
 
     exp_dict_list = heat_2d_long_time_experiment(grid_res)
