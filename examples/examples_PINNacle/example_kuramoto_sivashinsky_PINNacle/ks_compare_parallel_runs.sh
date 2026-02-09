f1ca7d9673797625598c96c660cfd36524ebe121
Saitama32 <rostik743@gmail.com>
2026-02-09 23:45:38 +0300

    debug wrong path

--- examples/examples_PINNacle/example_kuramoto_sivashinsky_PINNacle/ks_compare_parallel_runs.sh
+++ examples/examples_PINNacle/example_kuramoto_sivashinsky_PINNacle/ks_compare_parallel_runs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Автоматический запуск kuramoto_sivashinsky_chain.py с распределением по GPU
 
-SCRIPT="examples/examples_PINNacle/example_KS_PINNacle/ks_rl_comparison.py"
+SCRIPT="examples/examples_PINNacle/example_kuramoto_sivashinsky_PINNacle/ks_rl_comparison.py"
 
 # Проверяем, сколько доступно GPU
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
