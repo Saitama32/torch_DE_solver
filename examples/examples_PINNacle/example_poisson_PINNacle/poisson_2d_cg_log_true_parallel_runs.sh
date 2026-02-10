e0a2738dc4deafc456c0a1a6e86dd7ae599e65cd
Saitama32 <rostik743@gmail.com>
2026-02-10 13:12:44 +0300

    Change keys

--- examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_cg_log_true_parallel_runs.sh
+++ examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_cg_log_true_parallel_runs.sh
@@ -8,8 +8,8 @@ NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
 log_enable="True"
 log_unenable="False"
-exp_key_1="5358eb806ed54af8b1d37bde87f3dde3"
-exp_key_2="e0fb7ca627b54476b6385992217f2531"
+exp_key_1="1c8b7c85f97a415e880666ffb7ad7411"
+exp_key_2="77e57523c53b43f486017e432eff8d22"
 exp_key_3=""
 exp_key_4=""
 
