e5cb12e345d0554bba74c88a3f30032bce3355fc
Saitama32 <rostik743@gmail.com>
2026-02-16 16:25:06 +0300

    Change keys for poisson ms

---
+++ examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_ms_parallel_runs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Автоматический запуск poisson_2d_cg_chain.py с распределением по GPU
+
+SCRIPT="examples/examples_PINNacle/example_poisson_PINNacle/poisson_2d_ms_pinncale_comparison.py"
+
+# Проверяем, сколько доступно GPU
+NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+
+log_enable="True"
+log_unenable="False"
+exp_key_1="a0af3d6c28084350a0510e3a5f6d7858"
+exp_key_2="b893ceac0e9c40778f4a9b3ccce76769"
+echo "Обнаружено GPU: $NUM_GPUS"
+
+if [ "$NUM_GPUS" -eq 0 ]; then
+    echo "❌ Не найдено ни одного CUDA-устройства. Выходим."
+    exit 1
+fi
+
+if [ "$NUM_GPUS" -eq 1 ]; then
+    echo "Запускаем 2 процесса на одной GPU..."
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" --log_key "$log_unenable" --exp_key "$exp_key_1"&
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" --log_key "$log_unenable" --exp_key "$exp_key_2"&
+elif [ "$NUM_GPUS" -ge 2 ]; then
+    echo "Запускаем по 2 процесса на каждую из двух GPU..."
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" --log_key "$log_unenable" --exp_key "$exp_key_1"&
+    CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" --log_key "$log_unenable" --exp_key "$exp_key_2"&
+    # CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" --log_key "$log_enable" --exp_key "$exp_key_3"&
+    # CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" --log_key "$log_enable" --exp_key "$exp_key_4"&
+else
+    echo "⚠️ Найдено более 2 GPU, но используется только первые две."
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
+    CUDA_VISIBLE_DEVICES=0 python "$SCRIPT" &
+    CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
+    CUDA_VISIBLE_DEVICES=1 python "$SCRIPT" &
+fi
+
+# Ждём завершения всех процессов
+wait
+echo "✅ Все процессы завершены."
